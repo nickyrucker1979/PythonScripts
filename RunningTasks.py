72a8b4f773001fdbe3ed9320ec95f293a1569e44
Nicky Rucker <nickyrucker@hotmail.com>
2018-11-13 10:46:52 -0700

    Update RunningTasks.py

---
+++ RunningTasks.py
@@ -0,0 +1,31 @@
+import os
+
+def getTasks(name):
+    r = os.popen('tasklist /v').read().strip().split('\n')
+    print ('# of tasks is %s' % (len(r)))
+    for i in range(len(r)):
+        s = r[i]
+        if name in r[i]:
+            print ('%s in r[i]' %(name))
+            return r[i]
+    return []
+
+if __name__ == '__main__':
+    '''
+    This code checks tasklist, and will print the status of a code
+    '''
+
+    imgName = 'dwm.exe'
+
+    notResponding = 'Not Responding'
+
+    r = getTasks(imgName)
+
+    if not r:
+        print('%s - No such process' % (imgName))
+
+    elif 'Not Responding' in r:
+        print('%s is Not responding' % (imgName))
+
+    else:
+        print('%s is Running or Unknown' % (imgName))
